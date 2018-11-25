6b89793c5850816c524689adec828322623df82d
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-25 14:53:26 +0100

    Update run.sh

--- strlength/run.sh
+++ strlength/run.sh
@@ -4,12 +4,11 @@
 
 make  
 
-res1=$(date +%s.%N)
+res1=$(date +%N)
 ./"$1" 
-res2=$(date +%s.%N)
+res2=$(date +%N)
 
-# dt=$(($res2 - $res1))
-let "dt=$res2-$res1"
+dt=$(($res2 - $res1))
 echo $dt
 
 #curl -s -F file=@strlength.h http://hepl.info-etude.be/test.php?result=$result -o 'output'
