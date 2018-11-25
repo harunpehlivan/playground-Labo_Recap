815c750cc561651df4e33af45cc8a2377b4e595a
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2018-11-25 14:50:03 +0100

    Update run.sh

--- strlength/run.sh
+++ strlength/run.sh
@@ -8,8 +8,8 @@ res1=$(date +%s.%N)
 ./"$1" 
 res2=$(date +%s.%N)
 
-dt=$(($res2 - $res1))
-
+# dt=$(($res2 - $res1))
+dt=$(echo "$res2+$res1" |bc )
 echo $dt
 
 #curl -s -F file=@strlength.h http://hepl.info-etude.be/test.php?result=$result -o 'output'
